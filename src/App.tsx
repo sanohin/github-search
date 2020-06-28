0a10baaa370e5c81db794eead493a4ea844a99e5
Aleksandr Anokhin <sasankhin124@gmail.com>
2020-06-28 22:30:01 +0300

    chore: add basic search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import * as React from "react";
+import { Search } from "./repositories/Search";
+import { AppLayout } from "./ui/AppLayout";
+import { RepositoriesList } from "./repositories/RepositoriesList";
+import { Progress } from "./ui/Progress";
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppLayout>
+      <Search value={searchValue} onChange={setSearchValue} />
+      <React.Suspense fallback={<Progress />}>
+        <RepositoriesList searchValue={searchValue} />
+      </React.Suspense>
+    </AppLayout>
   );
 }
 
---
+++ src/ui/Progress.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { Text } from "rebass";
+
+// todo: replace with something more beautiful
+export const Progress = () => {
+  return <Text>Loading...</Text>;
+};
